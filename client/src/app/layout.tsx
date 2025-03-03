11eacb86f17045703808c1361a5223a291d51309
AbrarShahriar <AbrarShahriar321@gmail.com>
2025-03-04 00:06:41 +0600

    fix: beam fix

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -4,7 +4,7 @@ import NextTopLoader from "nextjs-toploader";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { Toaster } from "@/components/ui/toaster";
-import Script from "next/script";
+// import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Mochapi",
@@ -19,13 +19,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`antialiased font-geist`}>
-        <Script
+        {/* <Script
           src="https://beamanalytics.b-cdn.net/beam.min.js"
           data-token={process.env.BEAM_ANALYTICS_TOKEN as string}
           async
           id="important-script"
           strategy="beforeInteractive"
-        />
+        /> */}
+        <script
+          src="https://beamanalytics.b-cdn.net/beam.min.js"
+          data-token="00b1075c-e776-4b86-90d0-0b44d88531d3"
+          async
+        ></script>
         <ClerkProvider appearance={{ baseTheme: dark }}>
           <NextTopLoader showSpinner={false} />
           {children}
