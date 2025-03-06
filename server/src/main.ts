026fa40032d87159c5cbc63687cabee1d7fc57fc
AbrarShahriar <AbrarShahriar321@gmail.com>
2025-03-07 03:41:42 +0600

    feat: manual trigger

--- server/src/main.ts
+++ server/src/main.ts
@@ -30,6 +30,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3001, () => console.log('Nest started'));
 }
 bootstrap();
